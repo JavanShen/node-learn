0971766728712b1b5974c74c89b1774c9f3e9ea2
apine <1985486089@qq.com>
2022-11-02 22:29:55 +0800

    :art: 格式化代码

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,23 +1,23 @@
-import { defaultTheme, defineUserConfig } from "vuepress";
-import { getDirname, path } from "@vuepress/utils";
-import { sidebar } from "./configs";
+import { defaultTheme, defineUserConfig } from 'vuepress'
+import { getDirname, path } from '@vuepress/utils'
+import { sidebar } from './configs'
 
-const __dirname = getDirname(import.meta.url);
+const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
-  lang: "zh-CN",
-  title: "Nodo.js笔记",
-  markdown: {
-    importCode: {
-      handleImportPath: (str) =>
-        str.replace(/^@/, path.resolve(__dirname, "../../code")),
+    lang: 'zh-CN',
+    title: 'Nodo.js笔记',
+    markdown: {
+        importCode: {
+            handleImportPath: str =>
+                str.replace(/^@/, path.resolve(__dirname, '../../code'))
+        }
     },
-  },
-  theme: defaultTheme({
-    locales: {
-      "/": {
-        sidebar,
-      },
-    },
-  }),
-});
+    theme: defaultTheme({
+        locales: {
+            '/': {
+                sidebar
+            }
+        }
+    })
+})
