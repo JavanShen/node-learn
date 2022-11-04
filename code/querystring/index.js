92f97d9c4c47afdc547c7b24750e44c7074f63d2
apine <1985486089@qq.com>
2022-11-04 23:20:17 +0800

    :hammer: 更新示例代码

--- code/querystring/index.js
+++ code/querystring/index.js
@@ -2,15 +2,25 @@ import querystring from 'querystring'
 
 const str = 'name=one&age=18&location=china'
 
+const cstr = 'id=2&city=北京&url=https://www.app.com'
+
 const myObj = {
     a: 1,
     b: 2,
     c: 3
 }
 
+// { name: 'one', age: '18, location: 'china'}
 const obj = querystring.parse(str)
 
+// a=1&b=2&c=3
 const myStr = querystring.stringify(myObj)
+
+// 编码和解码，可以防止某些注入
+const escaped = querystring.escape(cstr)
+const unescaped = querystring.unescape(escaped)
 
 console.log(obj)
 console.log(myStr)
+console.log(escaped)
+console.log(unescaped)
--- code/url/index.js
+++ code/url/index.js
@@ -6,6 +6,11 @@ export default function tryUrl(path = '/home?a=12&b=5') {
     const myUrl = new URL(path, baseUrl)
 
     console.log(url.parse(path, true))
+    console.log(myUrl)
+
+    console.log(myUrl.searchParams.get('a'))
+
+    console.log(url.format(myUrl))
 
     myUrl.searchParams.forEach((item, key) => {
         console.log(item, key)
